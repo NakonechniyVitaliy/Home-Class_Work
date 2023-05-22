85d4575e76b82b769ca87b5cecad2c5bcf09468f
Vitaliy <vetalpro30@gmail.com>
2023-05-23 01:21:48 +0300

    HomeWork 21.05.2023

--- main.py
+++ main.py
@@ -1,82 +1,63 @@
-class Employed():
+class Car:
 
-    def __init__(self, name, age):
+    def __init__(self, model, year, volume, color, price):
+        self.model = model
+        self.year = year
+        self.volume = volume
+        self.color = color
+        self.price = price
+
+    def show(self):
+        return(f"\nМодель: {self.model} \n"
+               f"Год: {self.year} \n"
+               f"Обьём: {self.volume} \n"
+               f"Цвет: {self.color} \n"
+               f"Цена: {self.price}")
+
+
+car_1 = Car("BMW", "2000", "500", "Black", "5000$")
+print(car_1.show())
+
+################################################################################
+
+class Book:
+
+    def __init__(self, name, year, publisher, genre, autor, price):
         self.name = name
-        self.age = age
-    def print_employed(self):
-        print("This is Employed class")
+        self.year = year
+        self.publisher = publisher
+        self.genre = genre
+        self.autor = autor
+        self.price = price
 
+    def show(self):
+        return(f"\nИмя: {self.name}\n"
+               f"Год: {self.year}\n"
+               f"Издатель: {self.publisher}\n"
+               f"Жанр: {self.genre}\n"
+               f"Автор: {self.autor}\n"
+               f"Цена: {self.price}")
 
+book_1 = Book("Flower", "2019", "Booooks", "Триллер", "Елена В.Г.", "25$")
+print(book_1.show())
 
-class President(Employed):
+################################################################################
 
-    def print_employed(self):
-        print(f"Президент {self.name} керує країною")
-    def __str__(self):
-        return f"Президент {self.name} керує країною"
-    def __int__(self):
-        return f"Вік - {self.age}"
+class Stadion:
 
+    def __init__(self,name,year,country,city,size):
+        self.name = name
+        self.year = year
+        self.country = country
+        self.city = city
+        self.size = size
 
-class Manager(Employed):
+    def show(self):
+        return(f"\nНазвание: {self.name}\n"
+               f"Год: {self.year}\n"
+               f"Страна: {self.country}\n"
+               f"Город: {self.city}\n"
+               f"Вместимость: {self.size}")
 
-    def print_employed(self):
-        print(f"Менеджер {self.name} керує робітниками")
-    def __str__(self):
-        return f"Менеджер {self.name} керує робітниками"
-    def __int__(self):
-        return f"Вік - {self.age}"
-
-class Worker(Employed):
-
-    def print_employed(self):
-        print(f"Робітник {self.name} виконує свою роботу")
-    def __str__(self):
-        return f"Робітник {self.name} виконує свою роботу"
-    def __int__(self):
-        return f"Вік - {self.age}"
-
-
-
-
-prezident = President("Петро",45)
-manager = Manager("Ігор",33)
-worker = Worker("Сергій",19)
-
-prezident.print_employed()
-manager.print_employed()
-worker.print_employed()
-print("\n Далі йде вивід через __str__\n")
-print(prezident.__str__(),"\n", prezident.__int__())
-print(manager.__str__(),"\n", manager.__int__())
-print(worker.__str__(),"\n", worker.__int__())
-
-#######################################################################
-
-class math:
-
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    @staticmethod
-    def plus(a, b):
-        return a + b
-
-    @staticmethod
-    def minus(a, b):
-        return a-b
-
-    @staticmethod
-    def dil(a, b):
-        return a/b
-
-    @staticmethod
-    def mno(a, b):
-        return a*b
-
-print("\nMath:")
-print(math.plus(20, 10))
-print(math.minus(20, 10))
-print(math.dil(20, 10))
-print(math.mno(20, 10))
+stadion_1 = Stadion("Донбасс Арена","2009","Украина","Донбас","50 000")
+print(stadion_1.show())
